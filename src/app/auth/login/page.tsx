bd5763f17d7b8b5ae175ba482054beb4c9f2b53f
Mujahidul Amin <mujahidulamin1108152@gmail.com>
2023-10-18 01:49:14 +0600

    login page added

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Button, Col, Row, message } from "antd";
-import loginImage from "../../../assets/login-pana.png";
+import loginImage from "../../../assets/Login-pana.png";
 import Image from "next/image";
 import Form from "@/components/Forms/Form";
 import FormInput from "@/components/Forms/FormInput";
@@ -33,61 +33,53 @@ const LoginPage = () => {
   };
 
   return (    
-    <Row
-      justify="center"
-      align="middle"
-      style={{
-        minHeight: "100vh",
-      }}
-    >
-      <Col className="hidden md:block" sm={12} md={16} lg={10}>
-        <Image src={loginImage} width={500} alt="login image" />
-      </Col>
-      <Col className="mx-10 md:mx-0" sm={12} md={8} lg={8}>
-        <h1 className="my-4 text-center text-xl">Login your account</h1>
-        <div>
-          <Form submitHandler={onSubmit}>
-            <div>
-              <FormInput name="email" type="email" size="large" label="Email" />
-            </div>
-            <div
-              style={{
-                margin: "15px 0px",
-              }}
-            >
-              <FormInput
-                name="password"
-                type="password"
-                size="large"
-                label="Password"
-              />
-              <div className="flex justify-end mt-1">
-                <a
-                  rel="noopener noreferrer"
-                  href="#"
-                  className="text-xs hover:underline text-gray-400 hover:text-gray-800"
-                >
-                  Forgot password?
-                </a>
+      <Row
+        justify="center"
+        align="middle"
+        style={{
+          minHeight: "100vh",
+        }}
+      >
+        <Col className="hidden md:block" sm={12} md={16} lg={10}>
+          <Image src={loginImage} width={500} alt="login image" />
+        </Col>
+        <Col className="mx-10 md:mx-0" sm={12} md={8} lg={8}>
+          <h1 className="my-4 text-center text-4xl font-bold">Login your account</h1>
+          <div>
+            <Form submitHandler={onSubmit}>
+              <div>
+                <FormInput name="email" type="email" size="large" label="Email" />
               </div>
-            </div>
-            <p className="px-6 text-sm mb-5 text-start dark:text-gray-400">
-              Don&rsquo;t have an account yet?{" "}
-              <Link
-                href="/auth/signUp"
-                className="hover:underline text-violet-400"
+              <div
+                style={{
+                  margin: "15px 0px",
+                }}
               >
-                Sign up
-              </Link>
-              .
-            </p>
-            <div className="flex justify-center">
-              <Button htmlType="submit">Login</Button>
-            </div>
-          </Form>
-        </div>
-      </Col>
-    </Row>
+                <FormInput
+                  name="password"
+                  type="password"
+                  size="large"
+                  label="Password"
+                />
+      
+              </div>
+              <p className="text-sm mb-5 text-start dark:text-gray-400">
+                Don&rsquo;t have an account yet?{" "}
+                <Link
+                  href="/auth/signUp"
+                  className="hover:underline text-blue-400"
+                >
+                  Sign up
+                </Link>
+                .
+              </p>
+              <div className="flex justify-start">
+                <Button htmlType="submit" type="primary">Login</Button>
+              </div>
+            </Form>
+          </div>
+        </Col>
+      </Row>
   );
 };
 

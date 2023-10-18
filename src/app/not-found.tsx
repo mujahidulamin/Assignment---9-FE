52dcac0085e07de41de86a70ce1b3590e46d0d41
Mujahidul Amin <mujahidulamin1108152@gmail.com>
2023-10-18 19:08:37 +0600

    not found page edited

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,16 +1,18 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import Image from "next/image";
+
 
 const NotFoundPage = () => {
   const router = useRouter();
   setTimeout(() => {
     router.push("/");
-  }, 3000);
+  }, 50000);
 
   return (
     <div className="hero h-full lg:h-[80vh] bg-base-100">
-    <div className="hero-content flex-col lg:flex-row">
+    <div className ="mt-20">
       <div>
         <h1 className="text-5xl font-bold">WE ARE SORRY,PAGE NOT FOUND!</h1>
         <p className="py-4 lg:max-w-2xl">
@@ -18,7 +20,12 @@ const NotFoundPage = () => {
           CHANGED OR IS TEMPORARILY UNAVAILABLE
         </p>
       </div>
-      <img src='https://i.ibb.co/sWwQ342/error.png' className="lg:max-w-lg rounded-lg" alt="" />
+      <Image   height={200}
+                  width={600}
+                  alt=""
+                  src='https://i.ibb.co/sWwQ342/error.png'
+                  className="w-[50%] rounded-lg  shadow-sm mx-auto"
+      />
     </div>
   </div>
   );

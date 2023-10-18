96fd604030ebb31c4ccf067db703f361f4ddc024
Mujahidul Amin <mujahidulamin1108152@gmail.com>
2023-10-19 01:36:53 +0600

    welcome to admin dashboard

--- src/app/(dashboardayout)/dashboard/admin/page.tsx
+++ src/app/(dashboardayout)/dashboard/admin/page.tsx
@@ -1,11 +1,10 @@
-import React from "react";
-import SetupAnalytics from "../../../../assets/SetupAnalytics.png";
-import Image from "next/image";
+
+
 
 const AdminPage = () => {
   return (
-    <div className="flex justify-center">
-      <Image src={SetupAnalytics} alt="" width={670} />
+    <div className="flex justify-center my-60">
+      <h1 className="lg:text-5xl text-2xl">Welcome To Admin Dashboard</h1>
     </div>
   );
 };

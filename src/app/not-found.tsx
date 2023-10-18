b8d4e5a9993d614060628639d87305ed383dfe6e
Mujahidul Amin <mujahidulamin1108152@gmail.com>
2023-10-18 18:57:27 +0600

    not found page added

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -9,11 +9,18 @@ const NotFoundPage = () => {
   }, 3000);
 
   return (
-    <div className="grid h-screen px-4 bg-white place-content-center">
-      <h1 className="tracking-widest text-gray-500 uppercase">
-        404 | Not Found
-      </h1>
+    <div className="hero h-full lg:h-[80vh] bg-base-100">
+    <div className="hero-content flex-col lg:flex-row">
+      <div>
+        <h1 className="text-5xl font-bold">WE ARE SORRY,PAGE NOT FOUND!</h1>
+        <p className="py-4 lg:max-w-2xl">
+          THE PAGE YOU ARE LOOKING FOR MIGHT HAVE BEEN REMOVED HAD ITS NAME
+          CHANGED OR IS TEMPORARILY UNAVAILABLE
+        </p>
+      </div>
+      <img src='https://i.ibb.co/sWwQ342/error.png' className="lg:max-w-lg rounded-lg" alt="" />
     </div>
+  </div>
   );
 };
 

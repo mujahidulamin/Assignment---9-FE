83419b334a463dd49ab3a5d4ca94335d357b02ac
Mujahidul Amin <mujahidulamin1108152@gmail.com>
2023-10-19 09:32:24 +0600

    faq create

--- src/app/(dashboardayout)/dashboard/admin/content/faqManagement/create/page.tsx
+++ src/app/(dashboardayout)/dashboard/admin/content/faqManagement/create/page.tsx
@@ -48,9 +48,6 @@ const CreateFaqPage = () => {
 
       <div>
         <h1 className="text-center text-4xl font-bold leadi">Add FAQ</h1>
-        <div className="max-w-[300px] mx-auto mt-3">
-          <hr className="border-t-1 border-gray-500" />
-        </div>
       </div>
 
       <div className="mt-10">

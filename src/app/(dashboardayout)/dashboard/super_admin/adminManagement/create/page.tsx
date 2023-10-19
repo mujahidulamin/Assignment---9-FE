1ccc1ed6af35c6b78844449699fd4f48d05b83b3
Mujahidul Amin <mujahidulamin1108152@gmail.com>
2023-10-19 09:53:46 +0600

    super admin create admin

--- src/app/(dashboardayout)/dashboard/super_admin/adminManagement/create/page.tsx
+++ src/app/(dashboardayout)/dashboard/super_admin/adminManagement/create/page.tsx
@@ -53,9 +53,6 @@ const CreateUserPage = () => {
 
       <div>
         <h1 className="text-center text-4xl font-bold leadi">Create Admin</h1>
-        <div className="max-w-[300px] mx-auto mt-3">
-          <hr className="border-t-1 border-gray-500" />
-        </div>
       </div>
 
       <div className="mt-10">
